1f49acdaf2d6e64e6bbf81929e687f27dc8b80df
Israel Lugo <hostelixisrael@gmail.com>
2017-09-27 13:20:46 -0400

    update(core/agente): add method Serialize to agentMessage

--- core/types/types.go
+++ core/types/types.go
@@ -1,9 +1,15 @@
 package types
 
+import "encoding/json"
+
 type AgentMessage struct {
 	AgentID     string `json:"agent_id"`
 	JobID       string `json:"job_id"`
 	ProcedureID string `json:"procedure_id"`
 	ActivityID  string `json:"activity_id"`
 	Status      string `json:"status"`
+}
+
+func (this *AgentMessage) Serialize() ([]byte, error) {
+	return json.Marshal(this)
 }
