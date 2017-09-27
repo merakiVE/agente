a38f7ce55f043009fd279a6e69ed8604e5a70650
Israel Lugo <hostelixisrael@gmail.com>
2017-09-27 18:25:20 -0400

    update(agente/managet): add new methods

--- core/pool/manager.go
+++ core/pool/manager.go
@@ -3,11 +3,14 @@ package pool
 import (
 	"github.com/garyburd/redigo/redis"
 	"github.com/gocraft/work"
+	"fmt"
 )
 
 type AgentManager struct {
 	pubSubConn *redis.PubSubConn
 	enqueuer   *work.Enqueuer
+
+	started bool
 }
 
 func (this *AgentManager) GetEnqueuer() *work.Enqueuer {
@@ -22,7 +25,11 @@ func (this *AgentManager) SubscribeChannel(channel string) error {
 	return this.pubSubConn.PSubscribe(channel)
 }
 
-func (this *AgentManager) Close() error {
+func (this *AgentManager) closeConnPubSub() error {
+	return this.closeConnPubSub()
+}
+
+func (this *AgentManager) Stop() error {
 	return this.pubSubConn.Close()
 }
 
@@ -30,9 +37,35 @@ func (this *AgentManager) ReceiveMessage() interface{} {
 	return this.pubSubConn.Receive()
 }
 
-func NewAgentManager(cnn_pub_sub *redis.Conn, cnn_pool *redis.Pool, namespace string) *AgentManager {
+func (this *AgentManager) Start() {
+
+	if !this.started {
+		return
+	}
+
+	this.started = true
+
+	this.initLoop()
+}
+
+func (this *AgentManager) initLoop() {
+	for {
+		switch v := this.ReceiveMessage().(type) {
+		case redis.Message:
+			fmt.Printf("%s: message: %s\n", v.Channel, v.Data)
+		case redis.PMessage:
+			fmt.Printf("%s: message: %s %s\n", v.Channel, v.Data, v.Pattern)
+		case redis.Subscription:
+			fmt.Printf("%s: %s %d\n", v.Channel, v.Kind, v.Count)
+		case error:
+			panic(v)
+		}
+	}
+}
+
+func NewAgentManager(cnn redis.Conn, cnn_pool *redis.Pool, namespace string) *AgentManager {
 	return &AgentManager{
-		pubSubConn: &redis.PubSubConn{Conn: cnn_pub_sub},
+		pubSubConn: &redis.PubSubConn{Conn: cnn},
 		enqueuer:   work.NewEnqueuer(namespace, cnn_pool),
 	}
 }
