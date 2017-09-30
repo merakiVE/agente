32d84582bd86fa423b7307d58f330bde5a9832dc
Israel Lugo <hostelixisrael@gmail.com>
2017-09-30 11:33:46 -0400

    update(agente/pool): new types ProcedureRequest

--- core/pool/pool.go
+++ core/pool/pool.go
@@ -1,8 +1,28 @@
 package pool
 
+import (
+	"fmt"
+	"encoding/json"
+)
+
 const (
 	JOB_FINISH    = "FINISH"
 	JOB_PAUSED    = "PAUSED"
 	JOB_MANUAL    = "MANUAL"
 	JOB_AUTOMATIC = "JOB_AUTOMATIC"
+
+	CHANNEL_PROCEDURE_REQUEST = "pool.agents.request_procedure"
 )
+
+type ProcedureRequest struct {
+	procedure string `json:"procedure"`
+	params    map[string]interface{} `json:"params"`
+}
+
+func (this *ProcedureRequest) Serialize() ([]byte, error) {
+	return json.Marshal(this)
+}
+
+func CreateNameChannel(channel, group, action string) string {
+	return fmt.Sprintf("%s.%s.%s", channel, group, action)
+}
