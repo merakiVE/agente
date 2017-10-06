fc8398fc5808813f6133151be5b9ce6b7d735c6d
Israel Lugo <hostelixisrael@gmail.com>
2017-10-06 12:21:44 -0400

    update(agent/pool): rename field to struct ProcedureRequest

--- core/pool/pool.go
+++ core/pool/pool.go
@@ -1,7 +1,6 @@
 package pool
 
 import (
-	"fmt"
 	"encoding/json"
 )
 
@@ -15,14 +14,10 @@ const (
 )
 
 type ProcedureRequest struct {
-	procedure string `json:"procedure"`
-	params    map[string]interface{} `json:"params"`
+	IDProcedure string `json:"procedure"`
+	Params      map[string]interface{} `json:"params"`
 }
 
 func (this *ProcedureRequest) Serialize() ([]byte, error) {
 	return json.Marshal(this)
-}
-
-func CreateNameChannel(channel, group, action string) string {
-	return fmt.Sprintf("%s.%s.%s", channel, group, action)
 }
