19a9b4400f3d450052e64d8783b6271f3b09f81e
Israel Lugo <hostelixisrael@gmail.com>
2017-10-12 17:51:23 -0400

    update(agente/pool): remove type ProcedureRequest from pool.go

--- core/pool/pool.go
+++ core/pool/pool.go
@@ -1,23 +1,10 @@
 package pool
 
-import (
-	"encoding/json"
-)
-
 const (
 	JOB_FINISH    = "FINISH"
 	JOB_PAUSED    = "PAUSED"
 	JOB_MANUAL    = "MANUAL"
-	JOB_AUTOMATIC = "JOB_AUTOMATIC"
+	JOB_AUTOMATIC = "AUTOMATIC"
 
 	CHANNEL_PROCEDURE_REQUEST = "pool.agents.request_procedure"
 )
-
-type ProcedureRequest struct {
-	IDProcedure string `json:"procedure"`
-	Params      map[string]interface{} `json:"params"`
-}
-
-func (this *ProcedureRequest) Serialize() ([]byte, error) {
-	return json.Marshal(this)
-}
