f07e38990f2c918bb496c83024aa8cd96017bec7
Israel Lugo <hostelixisrael@gmail.com>
2017-10-18 18:16:03 -0400

    update(konios/pool: add new channel in pool

--- core/pool/pool.go
+++ core/pool/pool.go
@@ -6,5 +6,11 @@ const (
 	JOB_MANUAL    = "MANUAL"
 	JOB_AUTOMATIC = "AUTOMATIC"
 
-	CHANNEL_PROCEDURE_REQUEST = "pool.agents.request_procedure"
+	POOL               = "pool"
+	POOL_AGENTS        = "agents"
+	ACTION_NEW_REQUEST = "new_request_procedure"
+	ACTION_NEW_UPDATE  = "new_update"
+
+	CHANNEL_NEW_PROCEDURE_REQUEST = POOL + "." + POOL_AGENTS + "." + ACTION_NEW_REQUEST
+	CHANNEL_NEW_PROCEDURE_UPDATE  = POOL + "." + POOL_AGENTS + "." + ACTION_NEW_UPDATE
 )
